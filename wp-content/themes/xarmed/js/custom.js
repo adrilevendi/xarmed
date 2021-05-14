c2c06bd59c616e43127b548750539349f13a330b
Adri Levendi <adrilevendi@yahoo.com>
2021-05-14 20:48:33 +0200

    add js

--- wp-content/themes/xarmed/js/custom.js
+++ wp-content/themes/xarmed/js/custom.js
@@ -1,33 +1,44 @@
 function $(prop) {
-    return jQuery(prop);
+	return jQuery(prop);
 }
 
-jQuery(document).ready(function(){
-    console.log('sds');
+jQuery(document).ready(function() {
+	console.log('sds');
 
-    let homeArmsCarousel = $('.xcarousel__inner').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: '410px',
-        arrows:false,
-    });
+	let homeArmsCarousel = $('.xcarousel__inner').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: '410px',
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					centerMode: false
+				}
+			},
+		]
+	});
 
-    homeArmsCarousel.on('wheel', (function(e) {
-        e.preventDefault();  if (e.originalEvent.deltaY < 0) {
-          $(this).slick('slickNext');
-        } else {
-          $(this).slick('slickPrev');
-        }
-      }));
+	homeArmsCarousel.on('wheel', (function(e) {
+		e.preventDefault(); if (e.originalEvent.deltaY < 0) {
+			$(this).slick('slickNext');
+		} else {
+			$(this).slick('slickPrev');
+		}
+	}));
 
-      let proudctCarousel = $('#productCarousel').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true
+	let proudctCarousel = $('#productCarousel').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true
 
-        // centerMode: true,
-        // centerPadding: '410px',
-        // arrows:false,
-    });
+		// centerMode: true,
+		// centerPadding: '410px',
+		// arrows:false,
+	});
 })
